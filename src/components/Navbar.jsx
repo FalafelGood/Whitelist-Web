b78c7a2a09de0ad31a8784c2984d85a1990237ad
Hudson Leone <leoneht0@gmail.com>
2025-06-26 10:36:36 -0600

    improved navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
+import {useState} from 'react';
 import {Link} from 'react-router-dom'
 
+const buttons = ['Home', 'About', 'Support'];
+
 function Navbar({ title='Whitelist' }) {
+
+  const [activeBtn, setActiveBtn] = useState(0);
+
   return (
-    <nav className='navbar mb-12 shadow-lg bg-linear-to-r from-black to-white  text-neutral-content w-full'>
+    <nav className='navbar mb-12 shadow-lg bg-linear-to-r from-black to-white text-neutral-content w-full'>
       <img src="../src/assets/white-no-text.svg" width="50px" className='ml-4'/>
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
@@ -14,14 +20,19 @@ function Navbar({ title='Whitelist' }) {
         </div>
       </div>
 
+      {/* What does flex-1 do? */}
       <div className="flex-1 px-2 mx-2">
         <div className="flex justify-end">
-          <Link to='/' className='btn btn-neutral btn-sm rounded-btn'>
-              Home
-          </Link>
-          <Link to='/about' className='btn btn-sm rounded-btn'>
-              About
-          </Link>
+
+          {buttons.map((label, idx) => (
+            <Link to={`/${label === 'Home' ? '' : label}`} 
+              className={`btn btn-sm rounded-btn mr-2 ${activeBtn === idx ? 'btn-neutral' : ''}`}
+              onClick={()=> setActiveBtn(idx)}
+            >
+              {`${label}`}
+            </Link>
+          ))}
+          
         </div>
       </div>
 
