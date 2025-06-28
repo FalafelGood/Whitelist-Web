462af2db56a0ada40747918af0b8584435c0de12
Hudson Leone <leoneht0@gmail.com>
2025-06-28 08:10:08 -0600

    Fixed height of videoCards so videos with short names are still the same size

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,7 +5,7 @@ function VideoCard({ video }) {
   return (
     <div className="card bg-base-300 shadow-md p-4 m-8 hover:brightness-90 max-w-[425px]">
       <Link to={`/watch/?v=${video.id}`}>
-        <h2 className="card-title mb-2 line-clamp-3">{`${video.name}`}</h2>
+        <h2 className="card-title mb-2 line-clamp-3 h-[80px]">{`${video.name}`}</h2>
         <p className="mb-2">Channel name</p>
         <img src={`https://i.ytimg.com/vi/${video.id}/0.jpg`} width="400px" height="160px" className="rounded-xl"></img>
       </Link>
