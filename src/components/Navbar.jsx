50c578897529f9dfd063244bd8c4d27d4bd209ce
Hudson Leone <leoneht0@gmail.com>
2025-07-08 13:21:40 -0600

    Video player now resizes with window, but I've done it in a janky way

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const buttons = ['Home', 'About', 'Support'];
 
-function Navbar({ title='Whitelist (Beta!)' }) {
+function Navbar({ title='Whitelist (Demo!)' }) {
 
   const [activeBtn, setActiveBtn] = useState(0);
 
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,13 +1,15 @@
 function VideoPlayer({ videoId, width=640, height=390}) {
 
   return (
-    <iframe 
+    <iframe
+      // Use CSS styling to size iframe... This is admittedly a bit janky, but I couldn't get tailwind to do what I wanted.
+      style={{width: "80vw", height: "80vh"}}
       id="player" 
       type="text/html" 
-      width={`${width}`} 
-      height={`${height}`}
+      // width={`${width}`} 
+      // height={`${height}`}
       src={`https://www.youtube.com/embed/${videoId}`}
-      className="rounded-3xl shadow-xl"
+      className="rounded-3xl shadow-xl mt-4 ml-4 mr-4 mb-2"
       ></iframe>
   );
 }
