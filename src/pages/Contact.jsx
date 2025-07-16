bd615bb67b7592587e83184d82086d3b2f4dda40
Hudson Leone <leoneht0@gmail.com>
2025-07-16 16:41:21 -0600

    Added responsive flex to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,12 +4,12 @@ function Contact() {
   return (
     <>
       <div className="hero">
-        <div className="hero-content text-center flex-col min-h-[440px]">
-        <h1 className="text-6xl mb-4">Meet the team!</h1>
+        <div className="hero-content text-center flex-col min-h-[220px]">
+        <h1 className="text-6xl">Meet the team!</h1>
         </div>
       </div>
 
-      <div className="flex justify-center items-center gap-4">
+      <div className="flex md:flex-col lg:flex-row justify-center items-center gap-4">
 
         <ContactCard name="Dr. Hudson Leone" photo="../src/assets/avatars/hudson.jpg" title="Founder" blurb={"\"He's a little confused but he's got the spirit!\""} email="leoneht0@gmail.com" linkedin="https://www.linkedin.com/in/hudson-leone-62924b123" github="https://github.com/FalafelGood"/>
 
