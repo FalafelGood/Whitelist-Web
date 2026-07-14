0332e15419f49282ffd6b199f58bfa8ef9ac238a
Hudson Leone <leoneht0@gmail.com>
2026-07-14 14:45:06 -0600

    Attempt 3: Instead of using the vercel plugin in vite.config.js, enforce redirects in vercel.json. Note that this breaks 'vercel dev' since it appears to overrive whatever routing is going on in that environment. For development, do 'npm run vdev' which uses an empty config file

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import vercel from 'vite-plugin-vercel/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    vercel({
-      cleanUrls: false,
-      rewrites: [
-        { source: '/(.*)', destination: '/index.html' },
-      ]
-    })
   ],
 })
